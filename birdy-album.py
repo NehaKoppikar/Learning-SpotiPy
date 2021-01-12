a85e9dd7f6163a9c9011bdfa2ced1a7d4c526f06
Neha Koppikar <neha.koppikar1721@nmims.edu.in>
2021-01-12 09:39:33 +0530

    removed-secret-keys

--- birdy-album.py
+++ birdy-album.py
@@ -3,8 +3,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-cid = "3834026abc8f4c4ca4649552cada0b8f"
-secret = "c37ea7694e024e808e759263ca65b7b9"
+cid = ""
+secret = ""
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=cid, client_secret=secret))
